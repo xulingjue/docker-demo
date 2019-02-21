5654218e0f4e426a37c43ebd3273c56e369b0eb2
许凌绝 <lingjue.xu@qq.com>
2019-02-21 14:45:35 +0800

    修改k8s部署文件

--- src/main/java/com/maqv/demo/docker/controller/HelloController.java
+++ src/main/java/com/maqv/demo/docker/controller/HelloController.java
@@ -23,7 +23,7 @@ public class HelloController {
 
     @GetMapping("")
     public String sayHi() {
-        logger.info(String.format("hello %s new log!", fromSystem));
-        return String.format("hello %s new log!", fromSystem);
+        logger.info(String.format("hello %s new2 log!", fromSystem));
+        return String.format("hello %s new2 log!", fromSystem);
     }
 }
